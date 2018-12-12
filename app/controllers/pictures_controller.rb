f127c500b6f78668a6abdc92d2b702ae06687d7b
Gabriela <gab.jo.cuello@gmail.com>
2018-12-12 18:26:26 -0500

    Create relationship and associations between users and pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -22,6 +22,8 @@ class PicturesController < ApplicationController
     @picture.artist = params[:picture][:artist]
     @picture.url = params[:picture][:url]
 
+    @picture.user_id = current_user.id
+
 
     if @picture.save
     # if the picture gets saved, generate a get request to "/pictures" (the index)
