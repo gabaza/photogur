73a2fc4ee9bb86fca18f8abfb93e2bc6385d8205
Gabriela <gab.jo.cuello@gmail.com>
2018-12-12 16:00:54 -0500

    Create user controller with new and create methods and create signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pictures#index'
-
-  get 'pictures' => 'pictures#index'
 
 
-  post 'pictures' => 'pictures#create'
+    resources :pictures
+    resource :users
+    # resource :sessions, only: %i(new create destroy)
+
+    root 'pictures#index'
 
 
-  get 'pictures/new' => 'pictures#new'
-
-
-  get 'pictures/:id' => 'pictures#show'
-
-  get 'pictures/:id/edit' => "pictures#edit"
-  patch 'pictures/:id' => "pictures#update"
-
-  delete 'pictures/:id' => 'pictures#destroy'
 
 
 
